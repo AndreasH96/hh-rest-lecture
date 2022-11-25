6e5a479915baa8c7584f6e0d89630cafae673e47
AndreasH96 <andreashaggstrom96@gmail.com>
2022-11-25 17:15:35 +0100

    Changed to FastAPI

---
+++ app/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI,HTTPException, Response
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+
+class UserSchema(BaseModel):
+    name:str
+    age :int
+    company:str
+
+load_dotenv()
+port = os.getenv('PORT') if  os.getenv('PORT') else 5000
+db = {}
+application = FastAPI()
+
+
+@application.get("/")
+def getUserIds():
+    return list(db.keys())
+
+@application.get("/{uid}")
+def getUser(uid:str):
+    if(not uid.lower() in db.keys()):
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+    user = db.get(uid.lower())
+    return user
+
+@application.post("/",status_code=201)
+def addUser(user: UserSchema):
+
+    #Since we validated the body, we know that user.name is a string at all times
+    db[user.name.lower()] = user
+
+    return "Success"
+
+
+@application.put("/{uid}",status_code=200)
+def updateUser(uid:str,user: UserSchema,response:Response):
+
+    if (not uid.lower() in db.keys()):
+        response.status_code=201
+
+    # We can perform same operation as in the POST request
+    addUser(user)
+    return "Success"
+
+@application.delete("/{uid}")
+def deleteUser(uid:str):
+    if(not uid.lower() in db.keys()):
+        return "User not found",404
+    db.pop(uid.lower())
+    return "Success", 200
---
+++ app/test_main.py
@@ -0,0 +1,26 @@
+
+from fastapi.testclient import TestClient
+from .main import application
+import json
+
+
+client = TestClient(application)
+
+
+def test_getUserIds_empty():
+    response = client.get("/")
+    assert json.loads(response.content) == []
+
+def test_add():
+    body = {"name":"andreas","age":26,"company":"Devoteam"}
+    response =client.post("/",json=body)
+    assert response.status_code == 201
+
+def test_getUserIds_notEmpty():
+    response = client.get("/")
+    assert json.loads(response.content) == ['andreas']
+
+def test_schema_validation():
+    body = {"name":"andreas","age":"twentysix","company":"Devoteam"}
+    response =client.post("/",json=body)
+    assert response.status_code == 422
